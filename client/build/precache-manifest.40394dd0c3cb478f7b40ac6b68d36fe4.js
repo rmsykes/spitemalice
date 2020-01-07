c066a83c24aaae94392a0ffc217618d3f297afd6
Marshall Sykes <rmsykes91@gmail.com>
2020-01-07 12:18:27 -0500

    added npm package and modified gitignore

---
+++ client/build/precache-manifest.40394dd0c3cb478f7b40ac6b68d36fe4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5fe7fe94885611d7730a29016eccfdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dc411745982b523ddbd",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "2dc411745982b523ddbd",
+    "url": "/static/js/main.c771d6b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
