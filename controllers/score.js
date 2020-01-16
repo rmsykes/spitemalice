4466533d5caf6c2b73ecd1723bc946b133a9f9cd
Marshall Sykes <rmsykes91@gmail.com>
2020-01-16 12:05:18 -0500

    beginning work on score controller

--- controllers/score.js
+++ controllers/score.js
@@ -1,50 +1,17 @@
-/* Step 1 import express
- *
- */
+// IMPORT EXPRESS
 const express = require('express')
 
-/* Step 2
- *
- * Import the api files from the models
- *
- * TODO: change the file path to the models file you'll need to use.
- * TODO: rename this from `templateApi` to something more sensible (e.g:
- * `shopsAPI`)
- *
- * NOTE: You may need to import more than one API to create the 
- * controller you need.
- * 
- */
+// IMPORT API
 const templateApi = require('../models/template.js')
 
-/* Step 3 
- * 
- * Create a new router.
- *
- * the router will "contain" all the request handlers that you define in this file.
- * TODO: rename this from templateRouter to something that makes sense. (e.g:
- * `shopRouter`)
- */
+// CREATE ROUTER
 const templateRouter = express.Router()
 
-/* Step 4
- * 
- * TODO: Put all request handlers here
- */
+// HANDLERS REQUEST 
 
-/* Step 5
- *
- * TODO: delete this handler; it's just a sample
- */ 
-templateRouter.get('/', (req, res) => {
-  res.json(templateApi.getHelloWorldString())
-})
 
-/* Step 6
- *
- * Export the router from the file.
- *
- */
+
+// EXPORT ROUTER
 module.exports = {
   templateRouter
 }
